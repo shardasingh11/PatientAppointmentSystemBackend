855338909210d10e81a310d9e5e5c47027168926
Jugal Goswami <iamjugalgoswami@gmail.com>
2025-04-25 00:12:00 +0530

    fixing the logout issue

--- auth/schemas.py
+++ auth/schemas.py
@@ -6,4 +6,9 @@ from pydantic import BaseModel
 class TokenResponse(BaseModel):
     access_token: str
     expiry_time: datetime
+
+    class Config:
+        json_encoders = {
+            datetime: lambda v: v.isoformat() + "Z"
+        }
     
